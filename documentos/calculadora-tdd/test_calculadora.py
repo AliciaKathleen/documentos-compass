07bed9994a6f0d5888a19301fd0e1c5d4a90ddfb
Alicia <aliciakathleenp@gmail.com>
2025-05-16 15:18:12 -0300

    multiplicação e divisão

--- documentos/calculadora-tdd/test_calculadora.py
+++ documentos/calculadora-tdd/test_calculadora.py
@@ -9,9 +9,15 @@ class TestCalculadora:
     def test_adicao(self, calc):
         assert calc.adicionar(2, 3) == 5
         assert calc.adicionar(-1, 1) == 0
-        
+
     def test_subtracao(self, calc):
         assert calc.subtrair(5, 3) == 2
         assert calc.subtrair(10, 15) == -5
 
-    
+    def test_multiplicacao(self, calc):
+        assert calc.multiplicar(3, 4) == 12
+        assert calc.multiplicar(-2, 5) == -10
+
+    def test_divisao(self, calc):
+        assert calc.dividir(10, 2) == 5
+        assert calc.dividir(1, 2) == 0.5
