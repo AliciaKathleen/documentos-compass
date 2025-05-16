0cbf231dafbc88183b4245d992ae1051a4fcce85
Alicia <aliciakathleenp@gmail.com>
2025-05-16 15:19:04 -0300

    operações extras

--- documentos/calculadora-tdd/test_calculadora.py
+++ documentos/calculadora-tdd/test_calculadora.py
@@ -21,3 +21,15 @@ class TestCalculadora:
     def test_divisao(self, calc):
         assert calc.dividir(10, 2) == 5
         assert calc.dividir(1, 2) == 0.5
+
+    def test_divisao_por_zero(self, calc):
+        with pytest.raises(ValueError):
+            calc.dividir(10, 0)
+
+    def test_potencia(self, calc):
+        assert calc.potencia(2, 3) == 8
+        assert calc.potencia(5, 0) == 1
+
+    def test_raiz_quadrada(self, calc):
+        assert calc.raiz_quadrada(9) == 3
+        assert calc.raiz_quadrada(16) == 4
