3419b180b61c64fa3a635d6acc565af87ba5c187
Alicia <aliciakathleenp@gmail.com>
2025-05-16 15:17:42 -0300

    adição e subtração

--- documentos/calculadora-tdd/test_calculadora.py
+++ documentos/calculadora-tdd/test_calculadora.py
@@ -5,3 +5,13 @@ class TestCalculadora:
     @pytest.fixture
     def calc(self):
         return Calculadora()
+    
+    def test_adicao(self, calc):
+        assert calc.adicionar(2, 3) == 5
+        assert calc.adicionar(-1, 1) == 0
+        
+    def test_subtracao(self, calc):
+        assert calc.subtrair(5, 3) == 2
+        assert calc.subtrair(10, 15) == -5
+
+    
